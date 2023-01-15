c496aa81017052e51c35a6e6bb5c6a0b46518522
Daniel <gendik38@gmail.com>
2023-01-15 11:46:27 +0100

    adding products to Main.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Main from './components/Main';
 // import each component here
 
 function App() {
   return (
    <>
    <Header />
+   <Main />
    </>
   );
 }
