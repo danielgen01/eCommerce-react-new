8f93bae8febdd7ce29eb2344621d6115017d2f30
Daniel <gendik38@gmail.com>
2023-01-15 11:16:15 +0100

    Setup files: adding header

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+
+const Header = () => {
+    const [headline,setHeadline] = useState('++ ONLY TODAY SPECIAL OFFERS ! ++');
+
+    const headlines = ['++ STRAWBERRIES | BUY 3 - GET 10% OFF ++',
+     '++ FRUIT TEA | BUY ONE GET ONE FREE ++',
+    '++ ONLY TODAY SPECIAL OFFERS ! ++'];
+
+    useEffect(()=> {
+    let timeoutId;
+    const newHeadline = () => {
+
+    let randoNum = Math.floor(Math.random() * headlines.length);
+    let randomHeadline = headlines[randoNum];
+    setHeadline(randomHeadline)
+    timeoutId = setTimeout(newHeadline,5000)
+    }
+
+    newHeadline();
+     
+
+    
+    },[]);
+
+    
+    return (
+    <div className='Header'>
+        <h1> {headline}</h1>
+
+    </div>
+  )
+}
+
+export default Header
