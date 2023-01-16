f6f800d37f4d6808ca163e8fa9b8afc41e38f694
Daniel <gendik38@gmail.com>
2023-01-16 14:16:51 +0100

    add animation for hovering over products

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -30,12 +30,12 @@ const Main = ({isOpen,setIsOpen,toggleCart,cartItems,setCartItems}) => {
             {/* PRODUCT 1 */}
 
     <div className='product col-s-3 col-3'>
-        <h2>Fruit Tea</h2>
+        
         <img src='https://h2omart.com.au/wp-content/uploads/2021/08/fruit-tea-freeze.png' 
         className="product-img"
         
         />
-
+        <h2>Fruit Tea</h2>
         <h3>3.11€</h3>
 
         <input 
@@ -44,6 +44,7 @@ const Main = ({isOpen,setIsOpen,toggleCart,cartItems,setCartItems}) => {
         pattern="[1-9]"
         className='input-field'
         defaultValue="1"
+        
         
         />
 
@@ -58,10 +59,11 @@ const Main = ({isOpen,setIsOpen,toggleCart,cartItems,setCartItems}) => {
     {/* PRODUCT 2 */}
 
     <div className='product col-s-3 col-3'>
-        <h2>Strawberries</h2>
+        
         <img src='https://gwpriceltd.co.uk/wp-content/uploads/2020/04/strawberries-min.jpg' 
        className='product-img'
         />
+        <h2>Strawberries</h2>
 
         <h3>5.00€</h3>
 
@@ -85,11 +87,11 @@ const Main = ({isOpen,setIsOpen,toggleCart,cartItems,setCartItems}) => {
     {/* PRODUCT 3 */}
 
     <div className='product col-s-3 col-3'>
-        <h2>Coffee</h2>
+     
         <img src='https://m.media-amazon.com/images/I/41Qfhit6s3L._AC_.jpg' 
         className='product-img'
         />
-
+        <h2>Coffee</h2>
         <h3>11.23€</h3>
 
         <input 
